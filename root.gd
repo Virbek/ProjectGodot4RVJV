eeb13c85b070e57527b2c217d802cd5a4cc64006
Virbek <61600649+Virbek@users.noreply.github.com>
2024-10-01 14:20:24 +0200

    explosion réparer

--- root.gd
+++ root.gd
@@ -5,7 +5,6 @@ extends Node2D
 
 @onready var slime: CharacterBody2D = %slime
 @onready var sprite_slime: AnimatedSprite2D = $slime/AnimatedSprite2D
-@onready var poteau: StaticBody2D = $poteau
 
 
 const SPEED = 300 # px per second
@@ -20,16 +19,10 @@ var anim_direction_slime: String = "down"
 func _ready() -> void:
 	#EventBus.level_started.emit()
 	#EventBus.level_started.connect(func(): pass)
-	#var rng = RandomNumberGenerator.new()
-	#var rndX = rng.randi_range(0, 1000)
-	#var rndY = rng.randi_range(0, 1000)
-	#poteau.position = Vector2(rndX, rndY)
-	#poteau.visible = true;
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_dt: float) -> void:
-	
 	# movements player
 	var ix = Input.get_axis("player_left", "player_right")
 	var iy = Input.get_axis("player_up", "player_down")
@@ -56,26 +49,29 @@ func _physics_process(_dt: float) -> void:
 	sprite.flip_h = flip_x
 	
 	# movements slime
-	
-	var iv_slime = Vector2(player.position.x - slime.position.x,player.position.y - slime.position.y).normalized()
-	if slime.isSeeingPlayer():
-		direction_slime = iv_slime
-		slime.velocity = iv_slime * SPEED/2
-		slime.move_and_slide()
+	if(slime != null):
+		if(slime.PlayerIsInExploseRange() && !slime.isExplosing):
+			slime.startExplose()
+						
+		var iv_slime = Vector2(player.position.x - slime.position.x,player.position.y - slime.position.y).normalized()
+		if slime.isSeeingPlayer():
+			direction_slime = iv_slime
+			slime.velocity = iv_slime * SPEED/2
+			slime.move_and_slide()
+			
+		# animation
+		var base_anim_slime = "idle_" if !slime.isSeeingPlayer() else "move_"
+		var flip_x_slime = false
+		if direction_slime.y > 0.7:
+			anim_direction_slime = "down"
+		elif direction_slime.y < -0.7:
+			anim_direction_slime = "up"
+		elif direction_slime.x < -0.7:
+			anim_direction_slime = "right"
+			flip_x_slime = true
+		elif direction_slime.x > 0.7:
+			anim_direction_slime = "right"
 		
-	# animation
-	var base_anim_slime = "idle_" if !slime.isSeeingPlayer() else "move_"
-	var flip_x_slime = false
-	if direction_slime.y > 0.7:
-		anim_direction_slime = "down"
-	elif direction_slime.y < -0.7:
-		anim_direction_slime = "up"
-	elif direction_slime.x < -0.7:
-		anim_direction_slime = "right"
-		flip_x_slime = true
-	elif direction_slime.x > 0.7:
-		anim_direction_slime = "right"
-	
 		var animation_name_slime = base_anim_slime + anim_direction_slime
 		if(!slime.isExplosing):
 			sprite_slime.play(animation_name_slime)
