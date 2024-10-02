b5371a2ebab7181a1d605e699241f9037d4994ac
Meithal <hurin8888@gmail.com>
2024-10-02 09:13:59 +0200

    fix merge

--- root.gd
+++ root.gd
@@ -6,10 +6,7 @@ extends Node2D
 @onready var trap: Area2D = $trap
 @onready var immobilization_label: Label = $player/immobilization_timer
 
-# Connectez le piège au moment où le script est prêt
-@onready var trap: StaticBody2D = $trap
-
-const SPEED = 300  # px per second
+const SPEED = 300 # px per second
 
 var direction: Vector2
 var anim_direction: String = "down"
@@ -24,22 +21,10 @@ var immobilization_duration: float = 2.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	add_to_group("players")  # Ajoutez le joueur au groupe "players"
-	trap.connect("body_entered", _on_Trap_body_entered)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	# Compte le temps écoulé pour l'immobilisation
-	timer += delta
-
-	# Si le temps écoulé dépasse le délai et que le joueur n'est pas déjà immobilisé
-	if timer >= immobilize_delay and not is_immobilized and not has_been_immobilized:
-		immobilize_player()
-
-	# Si le joueur est immobilisé, gère la durée d'immobilisation
-	if is_immobilized:
-		if timer >= immobilize_delay + immobilize_duration:
-			free_player()
+	#EventBus.level_started.emit()
+	#EventBus.level_started.connect(func(): pass)
+	player.add_to_group("players")
+	trap.connect("player_immobilized", Callable(self, "_on_Trap_immobilize_player"))
 
 func _physics_process(_dt: float) -> void:
 	if not is_immobilized:
@@ -72,7 +57,7 @@ func _physics_process(_dt: float) -> void:
 	if(slime != null):
 		if(slime.PlayerIsInExploseRange() and not slime.isExplosing):
 			slime.startExplose()
-			
+						
 		var iv_slime = Vector2(player.position.x - slime.position.x,player.position.y - slime.position.y).normalized()
 		if slime.isSeeingPlayer():
 			direction_slime = iv_slime
@@ -110,21 +95,13 @@ func immobilize_player() -> void:
 
 # Fonction pour libérer le joueur après l'immobilisation
 func free_player() -> void:
-	if is_immobilized:  # Vérifie si le joueur est effectivement immobilisé
-		is_immobilized = false
-		player.set_physics_process(true)  # Réactive les mouvements du joueur
-		timer = 0.0  # Réinitialise le timer pour une éventuelle prochaine immobilisation
-		print("Le joueur peut bouger à nouveau !")
-		
-		# Rétablir la couleur d'origine
-		sprite.modulate = Color(1, 1, 1)  # Rétablit la couleur d'origine (blanc)
+	is_immobilized = false
+	player.set_physics_process(true)
+	sprite.modulate = Color(1, 1, 1)
+	immobilization_label.hide()
 
-# Fonction pour détecter le contact avec le piège
-func _on_Trap_body_entered(body: Node) -> void:
-	if body.is_in_group("players"):  # Vérifiez si le corps est le joueur
-		immobilize_player()  # Appelez la fonction d'immobilisation
-
-		#var animation_name_slime = base_anim_slime + anim_direction_slime
-		#if(!slime.isExplosing):
-		#	sprite_slime.play(animation_name_slime)
-		#sprite_slime.flip_h = flip_x_slime
+# Fonction appelée lorsque le signal est reçu
+func _on_Trap_immobilize_player() -> void:
+	immobilize_player()
+	await get_tree().create_timer(immobilization_duration).timeout
+	free_player()
