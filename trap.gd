6b52146be769383775f3f97098836055eaaba1b0
comeprz <come.przybyszewski@gmail.com>
2024-10-02 08:56:38 +0200

    UI délai trap ajoutée

--- trap.gd
+++ trap.gd
@@ -2,9 +2,9 @@ extends Area2D
 
 signal player_immobilized
 
-var is_active: bool = true  # Variable pour suivre l'état du piège
+var is_active: bool = true
 
-@onready var trap_sound: AudioStreamPlayer2D = $trap_sound  # Assurez-vous que le nœud est correctement référencé
+@onready var trap_sound: AudioStreamPlayer2D = $trap_sound
 
 func _ready() -> void:
 	connect("body_entered", Callable(self, "_on_Trap_body_entered"))
@@ -12,19 +12,16 @@ func _ready() -> void:
 # Fonction appelée lorsque le joueur entre dans l'Area2D
 func _on_Trap_body_entered(body: Node) -> void:
 	if body.is_in_group("players") and is_active:
-		# Jouer le son du piège
 		if trap_sound:
 			trap_sound.play()
 
-		# Émettre le signal d'immobilisation
 		emit_signal("player_immobilized")
 
-		# Désactiver le piège
 		deactivate_trap()
 
 # Fonction pour désactiver le piège
 func deactivate_trap() -> void:
-	is_active = false  # Mettre à jour l'état du piège
-	set_process(false)  # Désactiver le traitement du piège
-	set_physics_process(false)  # Désactiver les processus physiques
-	hide()  # Rendre le piège invisible
+	is_active = false
+	set_process(false)
+	set_physics_process(false)
+	hide()
